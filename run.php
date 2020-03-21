eede29d0c8483a4c37922b9f6a752ee6a31af6b5
ArSn <koljaz@web.de>
2020-03-21 22:42:05 +0100

    Added logging and and requesting the course overview. Also, adding parsing it from json.

--- run.php
+++ run.php
@@ -1,3 +1,51 @@
 <?php
 
-echo 'Done!' . PHP_EOL;
+require 'vendor/autoload.php';
+if (!file_exists('config.php')) {
+    logger('No config file found! Create a config.php based on config.example.php');
+    die;
+}
+$config = require('config.php');
+
+function logger($value) {
+    echo $value . PHP_EOL;
+}
+
+$jar = new \GuzzleHttp\Cookie\CookieJar;
+$client = new GuzzleHttp\Client([
+    'cookies' => $jar,
+]);
+$response = $client->request('GET', 'https://www.sgd-campus.de/api/v1/login/login', [
+    'headers' => [
+        'Host' => 'www.sgd-campus.de',
+        'username' => $config['user'],
+        'password' => $config['pass'],
+    ],
+]);
+
+if ($response->getStatusCode() == 200) {
+    logger('Login successful!');
+} else {
+    logger('Login not successful!');
+    die;
+}
+
+$response = $client->request('GET', 'https://www.sgd-campus.de/servlet/CurriculumOverview?display=main&cmd=3&userid=null&ilgid=332513');
+
+if ($response->getStatusCode() == 200) {
+    logger('Getting course overview successful!');
+} else {
+    logger('Getting course overview not successful!');
+    die;
+}
+
+$body = (string)$response->getBody();
+
+preg_match("/\'iCurriculumJSON\': (.*)/m", $body, $matches);
+
+$data = json_decode(trim(trim($matches[1]), ','), true);
+
+var_dump($data);
+
+
+logger('Done!');
