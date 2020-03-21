64113c7c0edc5191de0413fef6acc1c62cfe0a0a
ArSn <koljaz@web.de>
2020-03-21 23:29:51 +0100

    Extended example config to current state.

--- config.example.php
+++ config.example.php
@@ -3,4 +3,11 @@
 return [
     'user' => 'some-user',
     'pass' => 'some-pass',
+    'mail' => [
+        'host' => 'smtp.somedomain.com',
+        'port' => '587',
+        'user' => 'user@domain.com',
+        'pass' => 'some-password',
+        'recipient' => 'some-recipient@example.com',
+    ]
 ];
